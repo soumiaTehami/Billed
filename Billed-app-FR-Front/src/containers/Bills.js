09c3935d73e8221b57d5d182392ca64f1fabf228
tehami soumia <sou_mia_46@hotmail.fr>
2024-07-17 10:44:46 +0200

    erreur de tri

--- Billed-app-FR-Front/src/containers/Bills.js
+++ Billed-app-FR-Front/src/containers/Bills.js
@@ -33,7 +33,7 @@ export default class {
       .bills()
       .list()
       .then(snapshot => {
-        const bills = snapshot
+        const bills = snapshot.sort((a, b) => new Date(b.date) - new Date(a.date))
           .map(doc => {
             try {
               return {
